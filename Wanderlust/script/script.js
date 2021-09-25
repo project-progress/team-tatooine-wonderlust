410a0c42ec47d96c816b4ecf093e5e0ee5f05502
miqsargsyan21 <michael.sargs1an@gmail.com>
2021-09-25 05:09:20 +0400

    Added top attraction's API request

--- Wanderlust/script/script.js
+++ Wanderlust/script/script.js
@@ -1,6 +1,5 @@
-document.getElementsByTagName("button")[0].onclick = function () {
-    let city = document.getElementsByTagName("input")[0].value;
-    fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=5d5c5e800344c0d09a75889442acf66f&units=metric")
+function displayWeather (city) {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5d5c5e800344c0d09a75889442acf66f&units=metric")
     .then(response => response.json())
     .then(function(result){
         // input value we empty
@@ -34,19 +33,52 @@ document.getElementsByTagName("button")[0].onclick = function () {
         weatherInfo.appendChild(infoDate);
         weatherInfo.appendChild(infoTemp);
         weatherInfo.appendChild(infoCondition);
-        weatherInfo.appendChild(infoImg);
+        weatherInfo.appendChild(infoImg);   
+    })
+    .catch (() => {
+        document.getElementById("info").style.display = "none";
     });
-    fetch("https://api.foursquare.com/v2/venues/search?near= "+ city + " &client_id=CK2STORWRLE22ONGCMZ3PHAKMABVS0324RURA0KNT3M5JAAF&client_secret=WS00YPBBKPAQJLHBNO1YRYBCERTN3EB2MPFYQ5TI2C3GEJWH&v=20210924")
+}
+
+function displayAttractions (city) {
+    fetch("https://api.foursquare.com/v2/venues/explore?near=" + city + "&client_id=CK2STORWRLE22ONGCMZ3PHAKMABVS0324RURA0KNT3M5JAAF&client_secret=WS00YPBBKPAQJLHBNO1YRYBCERTN3EB2MPFYQ5TI2C3GEJWH&v=20210924")
     .then(response => response.json())
     .then(function(result){
-            console.log(result)
-        }
-        
-    );
+            let attractionDivs = document.getElementsByClassName('attr_info');
+            let res = result.response.groups[0].items;
+            for (let i = 0; i < attractionDivs.length; i++) {
+                let currentDiv = attractionDivs[i];
+                currentDiv.innerHTML = "";
 
+                let header = document.createElement('h3');
+                header.innerHTML = `<b>${res[i].venue.name}</b>`;
+                header.setAttribute('class', 'headerOfAttr')
+                currentDiv.appendChild(header);
+                
+                let img = document.createElement('img');
+                img.setAttribute('src', res[i].venue.categories[0].icon.prefix + "bg_64" + res[i].venue.categories[0].icon.suffix);
+                img.setAttribute('class', 'imgAttr')
+                currentDiv.appendChild(img);
+
+                let addr = document.createElement('address');
+                addr.innerHTML = `
+                    <span class = 'addrText'>Address:</span> <br>
+                    ${res[i].venue.location.address}<br>
+                    ${res[i].venue.location.city}<br>
+                    ${res[i].venue.location.country}
+                `;
+                addr.setAttribute('class', 'addrAttr');
+                currentDiv.appendChild(addr);
+            }
+        }  
+    )
+    .catch ((e) => {
+        alert('Input correct place name!');
+    });
 }
- 
-    
 
-
-
+document.getElementsByTagName("button")[0].onclick = function () {
+    let city = document.getElementsByTagName("input")[0].value;
+    displayWeather(city);
+    displayAttractions(city);
+}
